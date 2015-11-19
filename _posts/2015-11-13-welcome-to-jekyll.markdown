69b2ae075ea64e5b074900bdb7a2957a2551dc1e
pashaOliinyk <pashades@gmail.com>
2015-11-19 16:35:36 +0200

    add blog simple

---
+++ _posts/2015-11-13-welcome-to-jekyll.markdown
@@ -0,0 +1,21 @@
+---
+layout: post
+title:  "pasha test post"
+date:   2015-11-13 17:39:29 +0200
+categories: jekyll update
+links:
+ text: icon text
+ icon: [how-to, jekyll] 
+---
+# Structured documents
+
+Sometimes it's useful to have different levels of headings to structure your documents. Start lines with a `#` to create headings. Multiple `##` in a row denote smaller heading sizes.
+
+### This is a third-tier heading
+
+You can use  one `#` all the way up to `######` six for different heading sizes.
+
+If you'd like to quote someone, use the > character before the line:
+
+> Coffee. The finest organic suspension ever devised... I beat the Borg with it.
+> - Captain Janeway
---
+++ jasmine-test/spec/PlayerSpec.js
@@ -0,0 +1,95 @@
+describe("Player", function() {
+  var player;
+  var song;
+
+  beforeEach(function() {
+    player = new Player();
+    song = new Song();
+  });
+
+  it("should be able to play a Song", function() {
+    player.play(song);
+    expect(player.currentlyPlayingSong).toEqual(song);
+
+    //demonstrates use of custom matcher
+    expect(player).toBePlaying(song);
+  });
+
+  describe("when song has been paused", function() {
+    beforeEach(function() {
+      player.play(song);
+      player.pause();
+    });
+
+    it("should indicate that the song is currently paused", function() {
+      expect(player.isPlaying).toBeFalsy();
+
+      // demonstrates use of 'not' with a custom matcher
+      expect(player).not.toBePlaying(song);
+    });
+
+    it("should be possible to resume", function() {
+      player.resume();
+      expect(player.isPlaying).toBeTruthy();
+      expect(player.currentlyPlayingSong).toEqual(song);
+    });
+  });
+
+  // demonstrates use of spies to intercept and test method calls
+  it("tells the current song if the user has made it a favorite", function() {
+    spyOn(song, 'persistFavoriteStatus');
+
+    player.play(song);
+    player.makeFavorite();
+
+    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+  });
+
+
+  describe("Мой тест", function() {
+
+    it("summ test", function () {
+      expect(player.isPlaying).toBeFalsy();
+      player.summa(5, 3);
+      expect( player.pausedIn ).toEqual(8);
+
+      //console.log('this.pausedIn' , player.isPlaying , player.pausedIn);
+    });
+
+    it("сравнеение обьектов", function() {
+      var foo = {
+        a: 12,
+        b: 34
+      };
+      var bar = {
+        a: 12,
+        b: 34
+      };
+      expect(foo).toEqual(bar);
+
+      //console.log('this.pausedIn' , foo , bar);
+    });
+
+    it("The 'toMatch' matcher is for regular expressions", function() {
+      var message = "foo bar baz";
+
+      expect(message).toMatch(/bar/);
+      expect(message).toMatch("bar");
+      expect(message).not.toMatch(/quux/);
+
+      console.log('message' , message );
+    });
+
+  });
+
+  //demonstrates use of expected exceptions
+  describe("#resume", function() {
+    it("should throw an exception if song is already playing", function() {
+      player.play(song);
+
+      expect(function() {
+        player.resume();
+      }).toThrowError("song is already playing");
+    });
+  });
+});
