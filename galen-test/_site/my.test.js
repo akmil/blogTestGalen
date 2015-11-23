789807bb809519d610c4e7276e4194d52c27e886
pashaOliinyk <pashades@gmail.com>
2015-11-23 18:02:46 +0200

    css

---
+++ galen-test/_site/my.test.js
@@ -0,0 +1,16 @@
+/**
+ * not used
+ */
+test("SauceLabs test for homepage on iPhone", function () {
+    var driver = createGridDriver("http://ishubin:blabla-bla-bla-bla-blabla@ondemand.saucelabs.com:80/wd/hub", {
+        browser: "iphone",
+        desiredCapabilities: {
+            platform: "OS X 10.8",
+            version: "6.0",
+            "device-orientation": "portrait"
+        }
+    });
+
+    driver.get("http://testapp.galenframework.com");
+    checkLayout(driver, "home.spec", ["mobile"]);
+});
