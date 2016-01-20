211b36254bf48cb468b332dcd635d2d8eac20f2a
pashaOliinyk <pashades@gmail.com>
2016-01-20 12:59:54 +0200

    add: test click

---
+++ tests/galen/blog/menu/header-menu-modile.test.js
@@ -0,0 +1,47 @@
+test("Home page test on mobile device", function () {
+    var driver = createDriver("http://ticketmaster-api-staging.github.io/products-and-docs/apis/partner/",
+                              "320x800",
+                              "firefox");
+
+    var Blogs = $page("Blogs", {
+        title: "title",
+        closeButton: ".tm-close",
+        burgerButton: "#menu-btn"
+
+        /*load: function () {
+            this.open("http://ticketmaster-api-staging.github.io/products-and-docs/apis/partner/");
+            return this.waitToBeShown();
+        }*/
+    });
+
+    var blogs = new Blogs(driver);
+        blogs.burgerButton.click();
+
+    blogs.closeButton.waitFor(function (button) {
+        console.log('button.isDisplayed()' + button.isDisplayed());
+        return button.isDisplayed();
+    });
+
+    if (blogs.closeButton.exists()) {
+        console.log('blogs.closeButton.exists()' + blogs.closeButton.exists());
+        checkLayout({
+            driver: driver,
+            spec: "mobile-header-menu-open.gspec",
+            tags: ["mobile"]
+        });
+    }
+/*
+    GalenPages.wait( {time: "2s", period: "1s"}).untilAll({
+        "Password textfield should be shown": function () {
+            console.log('blogs.closeButton.isDisplayed()' + blogs.closeButton.isDisplayed());
+            return blogs.closeButton.isDisplayed();
+        }
+    });*/
+
+});
+
+
+
+/*
+galen test tests/galen/blog/menu/header-menu-modile.test.js --htmlreport tests/galen/reports/jsClick
+ */
