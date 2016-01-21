ed649461969fe71e71188a862f6ee1b06be06f98
pashaOliinyk <pashades@gmail.com>
2016-01-21 14:26:25 +0200

    add: click test in jquery3

--- tests/galen/blog/menu/open-burger.js
+++ tests/galen/blog/menu/open-burger.js
@@ -1,36 +1,7 @@
 /**
  * Created by Pavlo_Oliinyk1 on 1/21/2016.
  */
-//$("#menu-btn").click();
-
-var driver = createDriver("http://ticketmaster-api-staging.github.io/blogs",
-    "349x800",
-    "firefox"
-);
-
-var Blogs = $page("Blogs", {
-    title: "title",
-    closeButton: ".tm-close",
-    burgerButton: "#menu-btn"
-
-    /*load: function () {
-     this.open("http://ticketmaster-api-staging.github.io/products-and-docs/apis/partner/");
-     return this.waitToBeShown();
-     }*/
-});
-
-var blogs = new Blogs(driver);
-blogs.burgerButton.click();
-
-blogs.closeButton.waitFor(function (button) {
-    console.log('button.isDisplayed()' + button.isDisplayed());
-    checkLayout({
-        driver: driver,
-        spec: "./tests/galen/blog/menu/mobile-header-menu-open.gspec",
-        tags: ["mobile"]
-    });
-    return button.isDisplayed();
-});
+$("#menu-btn").click();
 
 /*
 galen check ./tests/galen/blog/menu/empty.gspec
